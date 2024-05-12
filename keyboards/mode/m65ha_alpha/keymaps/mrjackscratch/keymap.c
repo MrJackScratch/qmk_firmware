04d754f0595d3e9c8e3de6f366470e762d4580af
Mr. Jack <37601985+MrJackScratch@users.noreply.github.com>
2024-05-12 16:43:00 -0400

    Update keymap

--- keyboards/mode/m65ha_alpha/keymaps/mrjackscratch/keymap.c
+++ keyboards/mode/m65ha_alpha/keymaps/mrjackscratch/keymap.c
@@ -46,17 +46,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [GAM] = LAYOUT_65_ansi_blocker(
        KC_GRAVE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        MO(FNC), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_LALT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        MO(FNC), XXXXXXX, KC_LCTL,                            _______,                            KC_RGUI, _______, _______, _______, _______
+        KC_TAB , XXXXXXX, KC_LCTL,                            _______,                            KC_RGUI, _______, _______, _______, _______
     ),
 
 
     [FNC] = LAYOUT_65_ansi_blocker(
         KC_ESC,  KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX, KC_MUTE,
         XXXXXXX, KC_F5  , KC_F6  , KC_F7  , KC_F8  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,
-        _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_VOLD,
+        KC_CAPS, KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_VOLD,
         _______,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MO(SYS), XXXXXXX, KC_HOME,
         _______, _______, _______,                            KC_ENT,                             _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
     ),
